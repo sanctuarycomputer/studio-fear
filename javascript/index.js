d0dcaea27edf99e96b402c9ef92594a255f4bc66
Jenna Rothstein <rothstein.jenna@gmail.com>
2017-01-18 12:11:52 -0500

    Fixed it without Keith

--- javascript/index.js
+++ javascript/index.js
@@ -3,16 +3,21 @@ import _ from 'lodash';
 //import Waypoint from 'waypoints';
 
 window.onload = function (){
-  var menuButtonR= document.getElementById('rbutt');
-  var menuLinksR= document.getElementById('rlinks');
-    menuButtonR.addEventListener('click', () => {
-    menuLinksR.classList.toggle('is-active');
-  });
   var menuButtonL= document.getElementById('lbutt');
   var menuLinksL= document.getElementById('llinks');
     menuButtonL.addEventListener('click', () => {
     menuLinksL.classList.toggle('is-active');
+
   });
+
+  var menuButtonR= document.getElementById('rbutt');
+  var menuLinksR= document.getElementById('rlinks');
+  if (menuButtonR){
+    menuButtonR.addEventListener('click', () => {
+    menuLinksR.classList.toggle('is-active');
+  });
+}
+
 
   var $grid = $('.index-grid').isotope({
     itemSelector: '.index-grid-item',
